4219aa7e596708293a869215d295be4a86fd4258
Jerhemy Waldon <jswaldon@gmail.com>
2019-11-22 10:55:11 -0700

    Added Basic Character Controller and Map Builder

---
+++ Assets/RetroRPG/Scripts/CharacterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RetroRPG.Character
+{
+    public class CharacterController : MonoBehaviour
+    {
+        Vector3 _updatedVector = new Vector3();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //transform.position = _updatedVector = new Vector3(0, 0.5f, 0);
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            var rotation = transform.rotation;
+
+            if (Input.anyKey)
+            {
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    transform.transform.Rotate(0f, -90f, 0f);
+                }
+                else if (Input.GetKeyDown(KeyCode.E))
+                {
+                    transform.transform.Rotate(0f, 90f, 0f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    //transform.TransformDirection(Vector3.forward);
+                    transform.position += transform.forward;
+                     //this.transform.position = this.transform.position + (transform.forward + new Vector3(0,0,1));
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    //transform.TransformDirection(Vector3.back);
+                    transform.position += transform.forward * -1;
+                    //this.transform.position = this.transform.position + (transform. + new Vector3(0,0,1));
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position += transform.right * -1;
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position += transform.right;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/RetroRPG/Scripts/SlidingDoor.cs
@@ -0,0 +1,72 @@
+﻿//Make an empty GameObject and call it "Door"
+//Drag and drop your Door model into Scene and rename it to "Body"
+//Move the "Body" Object inside "Door"
+//Add a Collider (preferably SphereCollider) to "Door" Object and make it much bigger then the "Body" model, mark it as Trigger
+//Assign this script to a "Door" Object (the one with a Trigger Collider)
+//Make sure the main Character is tagged "Player"
+//Upon walking into trigger area the door should Open automatically
+
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour
+{
+    // Sliding door
+    public enum OpenDirection { x, y, z }
+    public OpenDirection direction = OpenDirection.y;
+    public float openDistance = 3f; //How far should door slide (change direction by entering either a positive or a negative value)
+    public float openSpeed = 2.0f; //Increasing this value will make the door open faster
+    public Transform doorBody; //Door body Transform
+
+    bool open = false;
+
+    Vector3 defaultDoorPosition;
+
+    void Start()
+    {
+        if (doorBody)
+        {
+            defaultDoorPosition = doorBody.localPosition;
+        }
+    }
+
+    // Main function
+    void Update()
+    {
+        if (!doorBody)
+            return;
+
+        if (direction == OpenDirection.x)
+        {
+            doorBody.localPosition = new Vector3(Mathf.Lerp(doorBody.localPosition.x, defaultDoorPosition.x + (open ? openDistance : 0), Time.deltaTime * openSpeed), doorBody.localPosition.y, doorBody.localPosition.z);
+        }
+        else if (direction == OpenDirection.y)
+        {
+            doorBody.localPosition = new Vector3(doorBody.localPosition.x, Mathf.Lerp(doorBody.localPosition.y, defaultDoorPosition.y + (open ? openDistance : 0), Time.deltaTime * openSpeed), doorBody.localPosition.z);
+        }
+        else if (direction == OpenDirection.z)
+        {
+            doorBody.localPosition = new Vector3(doorBody.localPosition.x, doorBody.localPosition.y, Mathf.Lerp(doorBody.localPosition.z, defaultDoorPosition.z + (open ? openDistance : 0), Time.deltaTime * openSpeed));
+        }
+    }
+
+    // Activate the Main function when Player enter the trigger area
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Collision!");
+            open = true;
+
+        }
+    }
+
+    // Deactivate the Main function when Player exit the trigger area
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            open = false;
+        }
+    }
+}
